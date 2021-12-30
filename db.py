00ae27c984156232782f5a3da60bc23c33828f18
Peter Kozyrev <63793503+PetrKozyrrev@users.noreply.github.com>
2021-12-30 10:54:47 +0300

    Add files via upload

--- db.py
+++ db.py
@@ -5,4 +5,10 @@ import sqlite3
 conn = sqlite3.connect("dtp_data.db")  # или :memory: чтобы сохранить в RAM
 cursor = conn.cursor()
 
+region = "Москва"
+datat = "2017"
 
+sql = f"SELECT SUM(dead_count),SUM(injured_count),COUNT(tags) FROM data WHERE region = '{region}' AND datetime LIKE '{datat}%'"
+cursor.execute(sql)
+a= cursor.fetchall()
+print(a[0][1])
